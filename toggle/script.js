bc5b89172bc733aaefcc4290c668933c8428dd92
sandrorios <sandroderios@hotmail.com>
2023-11-14 15:28:15 -0500

    practice

--- toggle/script.js
+++ toggle/script.js
@@ -1,11 +1,26 @@
+// let elements = document.querySelectorAll('.text');
+
+// for(element of elements){
+//     element.addEventListener('click', function() {
+//         if(!this.dataset.clicked){
+//             this.setAttribute('data-clicked', 'true');
+//             this.style.background = '#ccc';
+//             this.style.color = '#000';
+//         }else{
+//             this.removeAttribute('data-clicked');
+//             this.removeAttribute('style');
+//         }
+//     })
+// }
+
 let elements = document.querySelectorAll('.text');
 
 for(element of elements){
     element.addEventListener('click', function() {
-        if(!this.dataset.clicked){
+        if(!this.dataset.){
             this.setAttribute('data-clicked', 'true');
             this.style.background = '#ccc';
-            this.style.color = '#000';
+            this.style.color = '#000'
         }else{
             this.removeAttribute('data-clicked');
             this.removeAttribute('style');
