73923132383203f8b02621996643cfb5f2b3cac4
sandrorios <sandroderios@hotmail.com>
2023-11-30 06:15:07 -0500

    add new file for practice and added code to grab from the html

---
+++ listItem2/practice.js
@@ -0,0 +1,5 @@
+let formItem = document.getElementById('form-item');
+let listItem = document.getElementById('list-item');
+let inputField = document.getElementById('input-field');
+let filter = document.getElementById('filter');
+let clearBtn = document.getElementById('clear');
