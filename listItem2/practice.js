f8b73eddb840a9c7e4c8eff5b1415971e0a8d49b
sandrorios <sandroderios@hotmail.com>
2023-11-30 06:18:15 -0500

    added an alert

--- listItem2/practice.js
+++ listItem2/practice.js
@@ -3,3 +3,12 @@ let listItem = document.getElementById('list-item');
 let inputField = document.getElementById('input-field');
 let filter = document.getElementById('filter');
 let clearBtn = document.getElementById('clear');
+
+function addItem(e){
+    e.preventDefault();
+    let newItem = inputField.value;
+    if(newItem === ''){
+        alert("Please add an item");
+        return;
+    }
+}
